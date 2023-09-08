bb2c771ca8a85d86707b8d25891b405409d16eaa
B5Drago <34162672+bodyanich@users.noreply.github.com>
2023-09-08 14:17:30 +0300

    Update Program.cs

--- lab1/lab1/Program.cs
+++ lab1/lab1/Program.cs
@@ -1,63 +1,99 @@
-﻿namespace lab1
+namespace lab1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            using (StreamReader reader = new StreamReader("input.txt"))
+            DoTask();
+        }
+
+        static void DoTask()
+        {
+            try
             {
-                string? a = reader.ReadLine();
-                string? b = reader.ReadLine();
-
-                if (a is null || b is null)
+                using (StreamReader reader = new StreamReader("input.txt"))
                 {
-                    Console.WriteLine("One or both lines are null (end of file).");
-                    return;
-                }
+                    string? a = reader.ReadLine();
+                    string? b = reader.ReadLine();
 
-                int n = a.Length;
-                int m = b.Length;
-
-                int min_len = n + m;
-                for (int b_pos = -m; b_pos <= n; b_pos++)
-                {
-                    int begin = Math.Min(b_pos, 0);
-                    int end = Math.Max(b_pos + m - 1, n - 1);
-                    bool state = true;
-                    for (int j = begin; j <= end; j++)
+                    if (a is null || b is null)
                     {
-                        int a_val;
-                        if (j >= 0 && j < n)
-                            a_val = a[j] - '0';
-                        else
-                            a_val = 1;
+                        WriteError("One or both lines are null (end of file).");
+                        return;
+                    }
 
-                        int b_val;
-                        if (b_pos <= j && j < b_pos + m)
-                            b_val = b[j - b_pos] - '0';
-                        else
-                            b_val = 1;
+                    if (!IsNumeric(a) || !IsNumeric(b))
+                    {
+                        WriteError("Input lines should contain only numeric characters.");
+                        return;
+                    }
 
-                        if (a_val + b_val > 3)
+                    int n = a.Length;
+                    int m = b.Length;
+
+                    int min_len = n + m;
+                    for (int b_pos = -m; b_pos <= n; b_pos++)
+                    {
+                        int begin = Math.Min(b_pos, 0);
+                        int end = Math.Max(b_pos + m - 1, n - 1);
+                        bool state = true;
+                        for (int j = begin; j <= end; j++)
                         {
-                            state = false;
-                            break;
+                            int a_val;
+                            if (j >= 0 && j < n)
+                                a_val = a[j] - '0';
+                            else
+                                a_val = 1;
+
+                            int b_val;
+                            if (b_pos <= j && j < b_pos + m)
+                                b_val = b[j - b_pos] - '0';
+                            else
+                                b_val = 1;
+
+                            if (a_val + b_val > 3)
+                            {
+                                state = false;
+                                break;
+                            }
+                        }
+                        if (state)
+                        {
+                            int len = end - begin + 1;
+                            if (len < min_len)
+                                min_len = len;
                         }
                     }
-                    if (state)
+
+                    using (StreamWriter writer = new StreamWriter("output.txt"))
                     {
-                        int len = end - begin + 1;
-                        if (len < min_len)
-                            min_len = len;
+                        writer.Write(min_len);
                     }
-                }
 
-                using (StreamWriter writer = new StreamWriter("output.txt"))
-                {
-                    writer.Write(min_len);
+                    Console.WriteLine("Min length = " + min_len);
                 }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("An error occurred while reading/writing the file: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                WriteError("An unexpected error occurred: " + ex.Message);
+            }
+        }
 
-                Console.WriteLine("Min length = " + min_len);
+        static bool IsNumeric(string input)
+        {
+            return input.All(char.IsDigit);
+        }
+
+        static void WriteError(string errorMessage)
+        {
+            Console.WriteLine("Error: " + errorMessage);
+            using (StreamWriter errorWriter = new StreamWriter("output.txt"))
+            {
+                errorWriter.WriteLine("Error: " + errorMessage);
             }
         }
     }
